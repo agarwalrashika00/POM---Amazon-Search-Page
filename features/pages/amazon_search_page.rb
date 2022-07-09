d15bace94f58802196479a50e0abb72cb3834e4f
Rashika <agarwalrashika00@gmail.com>
2022-07-09 19:07:55 +0530

    made constants for html elements

--- features/pages/amazon_search_page.rb
+++ features/pages/amazon_search_page.rb
@@ -1,19 +1,21 @@
 class AmazonSearchPage
     include PageObject
+    SEARCH_BOX = {id: 'twotabsearchtextbox'}
+    SEARCH_BUTTON = {id: 'nav-search-submit-button'}
 
     def visit_site
         @browser.get 'http://www.amazon.com'
     end
 
     def search_for(search_key)
-        @browser.find_element(:id,'twotabsearchtextbox').send_keys search_key
+        @browser.find_element(SEARCH_BOX).send_keys search_key
     end
 
     def click_search_button
-        @browser.find_element(:id,'nav-search-submit-button').click
+        @browser.find_element(SEARCH_BUTTON).click
     end
 
     def search_result_present?(search_key)
-        @browser.find_element(:id,'twotabsearchtextbox').attribute('value') == search_key
+        @browser.find_element(SEARCH_BOX).attribute('value') == search_key
     end
 end
