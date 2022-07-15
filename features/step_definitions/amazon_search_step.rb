0378d7a5784983c901e6982010645e6b9ee4bd35
Rashika <agarwalrashika00@gmail.com>
2022-07-15 12:34:01 +0530

    added hooks AfterAll and BeforeAll

--- features/step_definitions/amazon_search_step.rb
+++ features/step_definitions/amazon_search_step.rb
@@ -1,12 +1,13 @@
 require_relative '../pages/amazon_search_page.rb'
 
 Given(/^a user goes to Amazon home page$/) do
-    @amazon_page = AmazonSearchPage.new(@browser)
+    @amazon_page = AmazonSearchPage.new($browser)
     @amazon_page.visit_site
 end
 
 When("user searches for {string}") do |keyword|
     @amazon_page.search_for(keyword)
+    sleep 1
     @amazon_page.click_search_button
 end
 
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,12 +1,12 @@
 require 'selenium-webdriver'
 require 'page-object'
 
-Before do |scenario|
-    @browser = Selenium::WebDriver.for :chrome
-    @browser.manage.window.maximize
-    @browser.manage.timeouts.implicit_wait = 10
+BeforeAll do |scenario|
+    $browser = Selenium::WebDriver.for :chrome
+    $browser.manage.window.maximize
+    $browser.manage.timeouts.implicit_wait = 10
 end
 
-After do |scenario|
-    @browser.quit
+AfterAll do |scenario|
+    $browser.quit
 end
